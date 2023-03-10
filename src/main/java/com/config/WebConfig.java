951e3cd9d939f268fb7a2cbbc5413097a80956b5
luo <luo@163.com>
2023-03-10 16:54:04 +0800

    23.3.10 重新测试各功能是否正常

--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.config;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,17 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${static.base-dir}")
+    private String baseDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String os = System.getProperty("os.name");
-        if (os.toLowerCase().startsWith("win")) {
-            registry.addResourceHandler("/img/**").
-                    addResourceLocations("file:E:/02-javaStu/Game_Community/img/");
-        } else {
-            //非win系统 可以根据逻辑再做处理;
-            registry.addResourceHandler("/img/**").
-                    addResourceLocations("file:/www/wwwroot/game_community/static/img/");
-        }
+        registry.addResourceHandler("/img/**").
+                addResourceLocations("file:" + baseDir + "img/");
     }
-
 }
