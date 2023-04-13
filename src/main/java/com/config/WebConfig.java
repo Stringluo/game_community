267f7878b8e0f8897c79c80b95a97e84239e30f7
luo <luo@163.com>
2023-04-13 21:21:49 +0800

    23.04.13重新测试

--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.config;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
